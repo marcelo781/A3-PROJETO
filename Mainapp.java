95be99cc00392ae12c60436b0d8d79c98b5a26d6
marcelo781 <39533676+marcelo781@users.noreply.github.com>
2023-06-19 10:48:41 -0300

    Add files via upload

---
+++ Mainapp.java
@@ -0,0 +1,31 @@
+package packapp;
+
+import perfil.Perfil;
+import projetoDAO.ProjetoDao;
+
+public class Mainapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ProjetoDao projetoDao = new ProjetoDao();
+		
+		
+		Perfil perfil = new Perfil();
+		perfil.setNomecliente("Marcelo");
+		perfil.setCpf (709,536,984,40);
+		perfil.setIdade(22);
+		perfil.setJogofavorito("Persona 5 Royal");
+		perfil.setNacionalidade("Brasileiro");
+		perfil.setQuantconquistas(45);
+		perfil.setTimedocoracao("Corinthians");
+		
+		projetoDao.save(perfil);
+		
+		
+		
+		
+	}
+
+}
