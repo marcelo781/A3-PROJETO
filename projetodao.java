9b7da004da2f7569b929eddea32e143b82a721c6
marcelo781 <39533676+marcelo781@users.noreply.github.com>
2023-06-21 10:27:38 -0300

    Update projetodao.java
    AGORA ELE FAZ DUAS FUNÇÕES DO CRUD: CREATE E READ.

--- projetodao.java
+++ projetodao.java
@@ -5,6 +5,13 @@ import java.sql.Connection;
 import factoryconection.Classeconectora;
 import perfil.Perfil;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.mysql.cj.jdbc.JdbcPreparedStatement;
+import com.mysql.cj.jdbc.PreparedStatementWrapper;
+import com.mysql.cj.protocol.Resultset;
 
 
 
@@ -19,9 +26,13 @@ public class ProjetoDao {
 	// D: DELETE
 	//
 	
-	public void save(Perfil perfil) {
 	
-	String sql = "INSERT INTO perfis(nome, cpf, idade, jogofav, nacionalidade, quantconquistas, timedocoracao) VALUES (?, ?, ?, ?, ?, ?, ? )";
+	
+	 //CREATE
+	public void save(Perfil perfil) {
+
+	
+	String sql = "INSERT INTO perfis(nome, idade, jogofav, nacionalidade, quantconquistas, timedocoracao) VALUES (?, ?, ?, ?, ?, ? )";
 	
 	Connection conn = null; 
 	
@@ -35,16 +46,17 @@ public class ProjetoDao {
 		pstm = (PreparedStatement) conn.prepareStatement(sql);
 		//Adicionando os valores que serão esperados pela query
 		pstm.setString(1, perfil.getNomecliente());
-		pstm.setInt(2, perfil.getCpf());
-		pstm.setInt(3, perfil.getIdade());
-		pstm.setString(4, perfil.getJogofavorito());
-		pstm.setString(5, perfil.getNacionalidade());
-		pstm.setInt(6, perfil.getQuantconquistas());
-		pstm.setString(7, perfil.getTimedocoracao());;
+		pstm.setInt(2, perfil.getIdade());
+		pstm.setString(3, perfil.getJogofavorito());
+		pstm.setString(4, perfil.getNacionalidade());
+		pstm.setInt(5, perfil.getQuantconquistas());
+		pstm.setString(6, perfil.getTimedocoracao());;
 	
 		// Executar a query
 		
 		pstm.execute();
+		
+		System.out.println("PERFIL SALVO COM SUCESSO, FERA!");
 	
 	}catch (Exception e) {
 		e.printStackTrace();
@@ -68,5 +80,78 @@ public class ProjetoDao {
 		
 		
 	}
-}
 	
+	//READ
+	public List<Perfil> getPerfis(){
+	
+		String sql = "SELECT * FROM perfis";
+		List<Perfil> perfis = new ArrayList<>();
+		
+		Connection conn = null;
+		JdbcPreparedStatement pstm = null;
+	
+//CLASSE PARA RECUPERAR OS DADOS SALVOS NO BANCO
+	
+		ResultSet rset = null;
+	
+	try {
+		conn = Classeconectora.createConnectionToMySQL();
+		pstm = (JdbcPreparedStatement) conn.prepareStatement(sql); 
+		rset = pstm.executeQuery();
+		
+		while (rset.next()) {
+			Perfil perfil = new Perfil();
+			
+// RECUPERAR O ID 
+		
+			perfil.setId(rset.getInt("id"));
+			
+// RECUPERAR O NOME 
+			
+			perfil.setNomecliente(rset.getString("nome"));
+			
+// RECUPERAR A IDADE
+			
+			perfil.setIdade(rset.getInt("idade"));
+			
+// RECUPERAR JOGO FAVORITO
+			
+			perfil.setJogofavorito(rset.getString("jogofav"));
+					
+// RECUPERAR NACIONALIDADE 
+			
+			perfil.setNacionalidade(rset.getString("nacionalidade"));
+		
+// RECUPERAR QT DE CONQUISTAS
+			
+			perfil.setQuantconquistas(rset.getInt("quantconquistas"));
+			
+// RECUPERAR TIME DO CORACAO
+			
+			perfil.setTimedocoracao(rset.getString("timedocoracao"));
+			
+			
+			perfis.add(perfil);
+			 
+		}	
+	}catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+		if (rset!=null) {
+			rset.close();
+			}
+		if (pstm!= null) {
+			pstm.close();
+		}
+		if (conn!= null) {
+			conn.close();
+		}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+}
+		return perfis; 
+		
+		}
+}
