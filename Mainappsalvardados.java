6eeb466f1dde65d8ef843d061b0e9e01ba4d3ba0
marcelo781 <39533676+marcelo781@users.noreply.github.com>
2023-06-22 10:45:01 -0300

    Update Mainappsalvardados.java

--- Mainappsalvardados.java
+++ Mainappsalvardados.java
@@ -1,5 +1,9 @@
 package packapp;
 
+import java.sql.Connection;
+
+import com.mysql.cj.jdbc.JdbcPreparedStatement;
+
 import perfil.Perfil;
 import projetoDAO.ProjetoDao;
 
@@ -24,11 +28,41 @@ public class Mainappsalvardados {
 		
 		projetoDao.save(perfil);
 		
+		// ATUALIZAR O CONTATO
+		
+		Perfil p1 = new Perfil();
+
+		p1.setNomecliente("João Henrique");
+		p1.setIdade(20);
+		p1.setJogofavorito("Call of Duty Modern Warfare 2");
+		p1.setNacionalidade("Brasileiro");
+		p1.setQuantconquistas(80);
+		p1.setTimedocoracao("Flamengo");
+		
+		p1.setId(7);  //NUMERO DO ID NO BANCO DE DADOS 
+		
+		projetoDao.update(p1);
+		
 		
 		// VISUALIZAÇÃO DOS REGISTROS NO BANCO 
 		
 		for(Perfil p : projetoDao.getPerfis());
 		System.out.println("Perfil: "+ perfil.getNomecliente());
-	}
+	
 
 }
+		
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+	
