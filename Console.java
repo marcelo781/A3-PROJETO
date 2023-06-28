5c08dd49e0a5b4314911b6bb22fee1e00c985e01
marcelo781 <39533676+marcelo781@users.noreply.github.com>
2023-06-28 00:13:00 -0300

    Add files via upload

---
+++ Console.java
@@ -0,0 +1,48 @@
+package console;
+
+public class Console {
+
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	public String nomeconsole;
+	public int idconsole;		
+	public int anodelançamento;
+	public String proprietaria;
+	public String getNomeconsole() {
+		return nomeconsole;
+	}
+	public void setNomeconsole(String nomeconsole) {
+		this.nomeconsole = nomeconsole;
+	}
+	public int getIdconsole() {
+		return idconsole;
+	}
+	public void setIdconsole(int idconsole) {
+		this.idconsole = idconsole;
+	}
+	public int getAnodelançamento() {
+		return anodelançamento;
+	}
+	public void setAnodelançamento(int anodelançamento) {
+		this.anodelançamento = anodelançamento;
+	}
+	public String getProprietaria() {
+		return proprietaria;
+	}
+	public void setProprietaria(String proprietaria) {
+		this.proprietaria = proprietaria;
+	}
+	
+	
+	
+	
+	
+	
+	
+		
+
+}
